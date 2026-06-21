5b08fd027ee6b0e68c13216f48e759a9d166047b
kimjensendk <68803500+kimjensendk@users.noreply.github.com>
2026-06-21 21:36:09 +0200

    Update config.js

--- config.js
+++ config.js
@@ -7,5 +7,5 @@
 //
 // Brug ALDRIG secret/service_role key i denne fil.
 
-const SUPABASE_URL = "https://DIN-PROJEKT-ID.supabase.co";
-const SUPABASE_PUBLISHABLE_KEY = "DIN_PUBLIC_KEY_HER";
+const SUPABASE_URL = "https://xlxcgjgwoxhzoybanoqr.supabase.co";
+const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_Nq6AsMiM9BKUOwuQB_A38w_oUXSSXzJ";

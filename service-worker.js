f984259e9837814eec049db4d4b90b3f46fbc969
kimjensendk <68803500+kimjensendk@users.noreply.github.com>
2026-06-21 20:36:31 +0200

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = "min-pwa-cache-v1";
+const FILES = [
+  ".",
+  "index.html",
+  "manifest.json",
+  "icon.svg"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(cached => {
+      return cached || fetch(event.request);
+    })
+  );
+});
